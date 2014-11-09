37f7690dd5d543193e44de52bbe9ea6e8a372990
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2014-11-08 22:22:58 -0500

    added code to find necessary trains from google maps given the trip details

--- app.py
+++ app.py
@@ -3,11 +3,18 @@ import json, urllib2
 
 app = Flask(__name__)
 
-url = "" #insert api url here
-request = urllib2.urlopen(url)
-result = request.read()
-d = json.loads(result) # a dictionary with api data stuff
+origin="Times+Square"
+destination="Penn+Station"
+arrival_time=1343641500
+mapsurl = "https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&arrival_time=%s&mode=transit&key=AIzaSyBWPZcD2dgi1T0F_dNC1SThe64a-rfdkgY"%(origin,destination,arrival_time)
+mapsrequest = urllib2.urlopen(mapsurl)
+mapsresult = mapsrequest.read()
+mapsD = json.loads(mapsresult)['routes'][0]['legs'][0]['steps'] # a dictionary with api data stuff
+for step in mapsD:
+    if step['travel_mode']=="TRANSIT":
+        train = step['transit_details']['line']['short_name']
+        print train
 
 if __name__=="__main__":
     app.debug = True
-    app.run()
+##    app.run()
